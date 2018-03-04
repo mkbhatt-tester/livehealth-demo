c094b3354e728a0b905d8ed9c798a783b6c54c2e
Maverick | Linux <mkbhatt99@gmail.com>
2018-03-05 02:19:28 +0530

    Update

--- livehealthapp/models.py
+++ livehealthapp/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
 
 class Note(models.Model):
 	note = models.ForeignKey(User)
-	note_title = models.CharField(max_length=20,unique=True)
+	note_title = models.CharField(max_length=50,unique=True)
 	note_body = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	shared_with = models.TextField()
